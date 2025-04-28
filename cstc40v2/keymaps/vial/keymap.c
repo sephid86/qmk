a5473a2d76c983b1f3f6d4df2f02de92a3c3b6f1
sephid86 <sephid86@gmail.com>
2025-04-28 13:44:06 +0900

    250428

--- cstc40v2/keymaps/vial/keymap.c
+++ cstc40v2/keymaps/vial/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
      * │Sft│ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │Sft│
      * ├───┼───┼───┼───┼───┼───┴───┼───┼───┼───┼───┼───┤
-     * │Ctl│Alt│GUI│NUM│FNC│  Spc  │   │NAV│Alt│App│Ctl│
+     * │Ctl│GUI│Alt│NUM│FNC│  Spc  │   │NAV│Alt│App│Ctl│
      * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
      */
     [_BASE] = LAYOUT_planck_mit(
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     DF(_NUMLCK),
         KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_ENT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
-        KC_LCTL,  KC_LALT,  KC_LGUI,  MO(_NUMLCK),  MO(_FUNC),  KC_SPC,  _______,  MO(_NAVI),  KC_RALT,  KC_APP,   KC_RCTL
+        KC_LCTL,  KC_LGUI,  KC_LALT,  MO(_NUMLCK),  MO(_FUNC),  KC_SPC,  _______,  MO(_NAVI),  KC_RALT,  KC_APP,   KC_RCTL
     ),
 
     /*
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
      */
     [_FUNC] = LAYOUT_planck_mit(
-        KC_GRV,     KC_F1,    KC_F2,    KC_F3,    KC_F4,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        KC_GRV,     KC_F1,    KC_F2,    KC_F3,    KC_F4,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,   KC_END,  _______,  _______,
         KC_TRNS,    KC_F5,    KC_F6,    KC_F7,    KC_F8,  _______,  KC_LEFT,  KC_DOWN,    KC_UP, KC_RIGHT,  KC_DQUO,  KC_TRNS,
         KC_TRNS,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  _______,  KC_MINS,   KC_EQL,  KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  _______,  _______,            KC_TRNS,  _______,  _______,  _______,  _______,  KC_RCTL
@@ -95,9 +95,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
      */
     [_NAVI] = LAYOUT_planck_mit(
-        QK_BOOT,   KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_BSPC,
-        KC_CAPS,  _______,  KC_PSCR,  _______,  _______,  _______,  _______,  KC_VOLU,  KC_HOME,  KC_PGUP, KC_QUOTE,  KC_TRNS,
-        KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,   KC_END,  KC_PGDN,    KC_UP,  KC_TRNS,
+        QK_BOOT,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_BSPC,
+        KC_CAPS,  _______,  KC_PSCR,  _______,  _______,  _______,  _______,  KC_VOLU,  _______,  _______, KC_QUOTE,  KC_TRNS,
+        KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  _______,  _______,    KC_UP,  KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  _______,  _______,            KC_TRNS,  _______,  _______,  KC_LEFT,  KC_DOWN, KC_RIGHT
     ),
     [_FN] = LAYOUT_planck_mit(
