3c08b16ca7c3c09b5445f72bcb7271b26fb4cafb
sephid86 <sephid86@gmail.com>
2025-04-28 13:57:06 +0900

    250428

--- cstc40v2/keymaps/sephid86/keymap.c
+++ cstc40v2/keymaps/sephid86/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
      * │Sft│ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │Sft│
      * ├───┼───┼───┼───┼───┼───┴───┼───┼───┼───┼───┼───┤
-     * │Ctl│GUI│Alt│NUM│FNC│  Spc  │   │NAV│Alt│App│Ctl│
+     * │Ctl│GUI│Alt│FNC│NUM│  Spc  │   │NAV│Alt│App│Ctl│
      * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
      */
     [_BASE] = LAYOUT_planck_mit(
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     DF(_NUMLCK),
         KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_ENT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
-        KC_LCTL,  KC_LGUI,  KC_LALT,  MO(_NUMLCK),  MO(_FUNC),  KC_SPC,  _______,  MO(_NAVI),  KC_RALT,  KC_APP,   KC_RCTL
+        KC_LCTL,  KC_LGUI,  KC_LALT,  MO(_FUNC),  MO(_NUMLCK),  KC_SPC,  _______,  MO(_NAVI),  KC_RALT,  KC_APP,   KC_RCTL
     ),
 
     /*
