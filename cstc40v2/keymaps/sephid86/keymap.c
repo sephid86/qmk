890d4783195d5c4ddfc6cde6c976afb22e342774
sephid86 <sephid86@gmail.com>
2025-04-28 11:37:53 +0900

    250428

--- cstc40v2/keymaps/sephid86/keymap.c
+++ cstc40v2/keymaps/sephid86/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_BASE] = LAYOUT_planck_mit(
         // KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     KC_BSPC,
-        QK_GESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     DF(_NUMLCK),
+        KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     DF(_NUMLCK),
         KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_ENT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
         KC_LCTL,  KC_LALT,  KC_LGUI,  MO(_NUMLCK),  MO(_FUNC),  KC_SPC,  _______,  MO(_NAVI),  KC_RALT,  KC_APP,   KC_RCTL
@@ -113,12 +113,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 // const key_override_t delete2_key_override = ko_make_basic(MOD_MASK_ALT, KC_SPC, KC_DEL);
 // const key_override_t tilde_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRV));
-// const key_override_t tilde_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, KC_TILD);
+const key_override_t tilde_key_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_ESC, KC_TILD);
 
 // This globally defines all key overrides to be used
-// const key_override_t *key_overrides[] = {
+const key_override_t *key_overrides[] = {
     // &delete_key_override,
     // &delete2_key_override,
-    // &tilde_key_override
-// };
+    &tilde_key_override
+};
 
