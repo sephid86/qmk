23b1e6301d22f2924ebd8d6d3ebf5816606dd05e
sephid86 <sephid86@gmail.com>
2025-08-27 12:40:22 +0900

    250827

--- cstc40/keymaps/vial/keymap.c
+++ cstc40/keymaps/vial/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     DF(_NUMLCK),
         KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_ENT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
-        KC_LCTL,  KC_LGUI,  KC_LALT,  MO(_FUNC),  MO(_NUMLCK),  KC_SPC,  _______,  MO(_NAVI),  KC_RALT,  KC_APP,   KC_RCTL
+        KC_LCTL,  KC_LGUI,  KC_LALT,  MO(_FUNC),  MO(_NUMLCK),  KC_SPC,  _______,  LT(_NAVI,KC_LNG1),  KC_RALT,  KC_APP,   KC_RCTL
     ),
 
     /*
@@ -95,14 +95,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
      */
     [_NAVI] = LAYOUT_planck_mit(
-        QK_BOOT,   KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR, KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_BSPC,
+        KC_TILD,   KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR, KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_BSPC,
         KC_CAPS,  _______,  KC_PSCR,  _______,  _______,  _______,  _______,  KC_VOLU,  _______,  _______, KC_QUOTE,  KC_TRNS,
-        KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  _______,  _______,  _______,  KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  _______,  _______,            KC_TRNS,  _______,  _______,  KC_LEFT,  KC_DOWN, KC_RIGHT
+        KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  _______,  _______,  KC_UP,  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  MO(_FN),  _______,            KC_TRNS,  _______,  _______,  KC_LEFT,  KC_DOWN, KC_RIGHT
     ),
     [_FN] = LAYOUT_planck_mit(
         // _______,  QK_BOOT,  DB_TOGG,  RM_TOGG,  RM_NEXT,  RM_HUEU,  RM_HUED,  RM_SATU,  RM_SATD,  RM_VALU,  RM_VALD,  KC_DEL,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        QK_BOOT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
@@ -110,15 +110,100 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-// const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-// const key_override_t delete2_key_override = ko_make_basic(MOD_MASK_ALT, KC_SPC, KC_DEL);
-// const key_override_t tilde_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRV));
-const key_override_t tilde_key_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_ESC, KC_TILD);
+// const key_override_t tilde_key_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_ESC, KC_TILD);
+// const key_override_t *key_overrides[] = {
+    // &tilde_key_override
+// };
 
-// This globally defines all key overrides to be used
-const key_override_t *key_overrides[] = {
-    // &delete_key_override,
-    // &delete2_key_override,
-    &tilde_key_override
-};
+// https://docs.qmk.fm/features/rgb_matrix#custom-rgb-matrix-effects
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (host_keyboard_led_state().caps_lock) {
+        for (uint8_t i = led_min; i < led_max; i++) {
+            if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
+                rgb_matrix_set_color(i, RGB_GOLD);
+            }
+        }
+    }
+
+// for (uint8_t i = led_min; i < led_max; i++) {
+        switch(get_highest_layer(layer_state|default_layer_state)) {
+            case _FUNC:
+                rgb_matrix_set_color(18, RGB_GOLDENROD);
+                rgb_matrix_set_color(19, RGB_GOLDENROD);
+                rgb_matrix_set_color(20, RGB_GOLDENROD);
+                rgb_matrix_set_color(21, RGB_GOLDENROD);
+                rgb_matrix_set_color(24, RGB_GOLDENROD);
+                rgb_matrix_set_color(25, RGB_GOLDENROD);
+                rgb_matrix_set_color(26, RGB_GOLDENROD);
+                rgb_matrix_set_color(27, RGB_GOLDENROD);
+                rgb_matrix_set_color(29, RGB_BLUE);
+                rgb_matrix_set_color(30, RGB_BLUE);
+                rgb_matrix_set_color(31, RGB_BLUE);
+                rgb_matrix_set_color(32, RGB_BLUE);
+                rgb_matrix_set_color(37, RGB_CYAN);//dd
+                rgb_matrix_set_color(38, RGB_CYAN);
+                rgb_matrix_set_color(39, RGB_CYAN);
+                rgb_matrix_set_color(40, RGB_CYAN);
+                rgb_matrix_set_color(42, RGB_GOLDENROD);
+                rgb_matrix_set_color(43, RGB_GOLDENROD);
+                rgb_matrix_set_color(44, RGB_GOLDENROD);
+                rgb_matrix_set_color(45, RGB_GOLDENROD);
+                break;
+            case _NUMLCK:
+                rgb_matrix_set_color(6, RGB_GREEN);
+                rgb_matrix_set_color(8, RGB_BLUE);
+                rgb_matrix_set_color(9, RGB_BLUE);
+                rgb_matrix_set_color(10, RGB_BLUE);
+                rgb_matrix_set_color(12, RGB_BLUE);
+                rgb_matrix_set_color(13, RGB_GREEN);
+                rgb_matrix_set_color(14, RGB_GREEN);
+                rgb_matrix_set_color(15, RGB_GREEN);
+                rgb_matrix_set_color(30, RGB_GREEN);
+                rgb_matrix_set_color(31, RGB_GREEN);
+                rgb_matrix_set_color(32, RGB_GREEN);
+                rgb_matrix_set_color(35, RGB_RED);
+                rgb_matrix_set_color(36, RGB_GREEN);
+                rgb_matrix_set_color(37, RGB_GREEN);
+                rgb_matrix_set_color(38, RGB_GREEN);
+                rgb_matrix_set_color(39, RGB_GREEN);
+                rgb_matrix_set_color(40, RGB_GREEN);
+                rgb_matrix_set_color(41, RGB_GREEN);
+                rgb_matrix_set_color(42, RGB_GREEN);
+                rgb_matrix_set_color(43, RGB_GREEN);
+                rgb_matrix_set_color(44, RGB_GREEN);
+                rgb_matrix_set_color(45, RGB_GREEN);
+                rgb_matrix_set_color(46, RGB_GREEN);
+                break;
+            case _NAVI:
+                rgb_matrix_set_color(8, RGB_BLUE);
+                rgb_matrix_set_color(9, RGB_BLUE);
+                rgb_matrix_set_color(10, RGB_BLUE);
+                rgb_matrix_set_color(12, RGB_BLUE);
+                rgb_matrix_set_color(15, RGB_PINK);
+                rgb_matrix_set_color(23, RGB_GOLD);
+                rgb_matrix_set_color(25, RGB_WHITE);
+                rgb_matrix_set_color(30, RGB_PINK);
+                break;
+            default:
+                break;
+        }
+    // }
+
+// if (get_highest_layer(layer_state) > 0) {
+//         uint8_t layer = get_highest_layer(layer_state);
+//
+//         for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+//             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+//                 uint8_t index = g_led_config.matrix_co[row][col];
+//
+//                 if (index >= led_min && index < led_max && index != NO_LED &&
+//                 keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+//                     rgb_matrix_set_color(index, RGB_GREEN);
+//                 }
+//             }
+//         }
+//     }
+
+    return false;
+}
 
